9ecc6c0a67d391150aa4dfe46e44224a1f6732fa
Darko <darkoshypka@gmail.com>
2026-02-24 23:53:10 +0100

    fix: resolved issues with numbers list

--- automate_python/Chapter 4 - Lists/commaCode.py
+++ automate_python/Chapter 4 - Lists/commaCode.py
@@ -1,14 +1,19 @@
 spam = ['apples', 'bananas', 'tofu', 'cats']
+spam2 = ['1', '22', '333', '4444']
+numbersList = [1, 2, 3, 4, 5]
 emptyList = []
 
 
 def commaFunction(words):
     for i in words:
         if i != words[-1]:
-            print(i + ',', end=" ")
+            print(str(i) + ',', end=" ")
 
         if i == words[-1]:
             print('and', words[-1], end=" ")
 
 
-commaFunction(emptyList)
+commaFunction(spam)
+# commaFunction(spam2)
+# commaFunction(numbersList)
+# commaFunction(emptyList)
