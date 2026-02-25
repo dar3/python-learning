e85700fd5fc09964b9d48af708074549e8519ce9
Darko <darkoshypka@gmail.com>
2026-02-25 21:26:38 +0100

    started creating coin game

---
+++ automate_python/Chapter 4 - Lists/coinFlip.py
@@ -0,0 +1,7 @@
+import random
+numberOfStreaks = 0
+for experimentNumber in range(10000):
+
+
+    # Code that checks if there is a streak of 6 heads or tails in a row.
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
