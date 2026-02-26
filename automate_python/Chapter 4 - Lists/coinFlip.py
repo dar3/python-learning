cb1cb49deb478bfa602747c171d2b12375d90f99
Darko <darkoshypka@gmail.com>
2026-02-26 23:22:46 +0100

    feat: add heads and tails counter

--- automate_python/Chapter 4 - Lists/coinFlip.py
+++ automate_python/Chapter 4 - Lists/coinFlip.py
@@ -1,7 +1,22 @@
 import random
+
 numberOfStreaks = 0
+coinSigns = []
+tailsCounter = 0
+headsCounter = 0
+
 for experimentNumber in range(10000):
+    ranNum = random.randint(0, 1)
+    if ranNum == 0:
+        coinSigns.append("T")
+        tailsCounter += 1
+    elif ranNum == 1:
+        coinSigns.append("H")
+        headsCounter += 1
 
 
-    # Code that checks if there is a streak of 6 heads or tails in a row.
 print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+print(coinSigns)
+print("There are: ", tailsCounter, "tails")
+print("There are: ", headsCounter, "heads")
+
