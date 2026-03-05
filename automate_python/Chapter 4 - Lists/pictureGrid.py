69a0bbf5d677d5e95792943e1db184962d440401
Darko <darkoshypka@gmail.com>
2026-03-05 14:24:15 +0100

    feat: pictureGrid app works

--- automate_python/Chapter 4 - Lists/pictureGrid.py
+++ automate_python/Chapter 4 - Lists/pictureGrid.py
@@ -8,14 +8,9 @@ grid = [['.', '.', '.', '.', '.', '.'],
 ['.', 'O', 'O', '.', '.', '.'],
 ['.', '.', '.', '.', '.', '.']]
 
+n = len(grid)
 
-# for x in range(6):
-#     print("x value is: ", x)
-#     print(grid[x][x])
-
-# grid[y][x]
-print(grid[2][5])
-
-# for y in range(len(grid)-1):
-#     for x in range(len(grid)-1):
-#         print(grid[x][y])
+for x in range(len(grid[0])):
+    for y in range(n):
+        print(grid[n - 1 - y][x], end='')
+    print()
